d126f066d5bcab4099e40b57fe1e33f9d8feb420
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-10-07 14:02:19 +0900

    add:Creating Footer

--- global/FooterBar.vue
+++ global/FooterBar.vue
@@ -1,10 +1,50 @@
 <template>
-<div>フッター</div>
+<div>
+  <v-footer
+      color="primary lighten-1"
+      padless
+  >
+    <v-row
+        justify="center"
+        no-gutters
+    >
+      <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+          class="primary lighten-1 py-4 text-center white--text"
+          padless
+          cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Favorite-Books-App</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</div>
 </template>
 
 <script>
 export default {
-  name: "FooterBar"
+  name: "FooterBar",
+  data(){
+    return{
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ],
+    }
+  }
 }
 </script>
 
